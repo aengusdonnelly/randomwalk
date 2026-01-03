0ccf1cc841443d24fecb1356d1a364e8ed7d8c58
aengus <aengus@kth.se>
2026-01-03 20:25:57 +0100

    mainly added a bias study

--- getdata.py
+++ getdata.py
@@ -1,6 +1,8 @@
+import numpy as np
 import randomwalk3d as rw
 
 def plot_walks():
+
     N = 100
 
     rw3d1 = rw.RandomWalk3D()
@@ -11,8 +13,15 @@ def plot_walks():
     rw3d2.generate(N, step=rw.Step.spherical1, crossing=False)
     rw3d2.plot()
 
+def bias_study():
+
+    N = 5000
+
+    rw.Step.scatter_steps(N, step=rw.Step.spherical2)
+
 def main():
-    plot_walks()
+
+    bias_study()
 
 if __name__ == "__main__":
     main()
--- randomwalk3d.py
+++ randomwalk3d.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as scs
 
 class Step():
 
@@ -26,6 +27,62 @@ class Step():
         dz = np.cos(theta)
 
         return dx, dy, dz
+    
+    def spherical2():
+        
+        while True:
+            x = np.random.uniform(-1, 1)
+            y = np.random.uniform(-1, 1)
+            z = np.random.uniform(-1, 1)
+
+            norm = np.linalg.norm(np.array((x, y, z)))
+
+            if norm > 1:
+                continue
+            else:
+                break
+
+        dx = x / norm
+        dy = y / norm
+        dz = z / norm
+        return dx, dy, dz
+
+    def scatter_steps(N, step=spherical1):
+
+        points = np.array(step())
+        for i in range(N-1):
+            points = np.vstack((points, np.array(step())))
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+
+        ax.scatter(points[:,0], points[:,1], points[:,2],
+                   color="red", s=1, label="Starting point")
+
+        ax.set_box_aspect((1, 1 ,1))
+        plt.show()
+
+        span = np.linspace(-1, 1, 500)
+
+        plt.plot(span, scs.gaussian_kde(points[:,0])(span),
+                 color="blue", linewidth=2, label="x values")
+        plt.plot(span, scs.gaussian_kde(points[:,1])(span),
+                 color="red", linewidth=2, label="y values")
+        plt.plot(span, scs.gaussian_kde(points[:,2])(span),
+                 color="black", linewidth=2, label="z values")
+        
+        plt.hist(points[:,0], bins=100, density=True,color="blue", alpha=0.5)
+        plt.hist(points[:,1], bins=100, density=True, color="red", alpha=0.5)
+        plt.hist(points[:,2], bins=100, density=True, color="black", alpha=0.5)
+
+        plt.legend()
+        plt.xlabel("Position")
+        plt.ylabel("Density")
+        plt.show()
 
 class RandomWalk3D():
 
@@ -85,9 +142,14 @@ class RandomWalk3D():
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
+
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_zticklabels([])
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+
         ax.scatter(self.path[0][0], self.path[0][1], self.path[0][2],
                    color="red", s=50, label="Starting point")
         
@@ -103,17 +165,5 @@ class RandomWalk3D():
 
         plt.legend()
         plt.title("3D random walk of length "+str(len(self.path)-1))
+        ax.set_box_aspect((1, 1 ,1))
         plt.show()
-
-    def scatter_steps(N, step=Step.spherical1):
-
-        points = []
-        for i in range(N):
-            points.append(step())
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        ax.set_zticklabels([])
-        ax.scatter(points, color="red", s=10, label="Starting point")
